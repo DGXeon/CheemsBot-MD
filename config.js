f7f92f0336294d80527cd1ec4336138d1de5caaf
The whatBots420 <105924903+Subbotsito@users.noreply.github.com>
2022-05-26 20:50:28 -0300

    Update config.js

--- config.js
+++ config.js
@@ -22,14 +22,14 @@ global.APIKeys = {
 }
 
 //═══════[modification]════════\\
-global.owner = ['916909137213','6283839980451'] //owner number, u can put multiple owner number, within quotations and seperated by comma.
-global.pemilik = ['916909137213'] //another owner number
-global.premium = ['916909137213'] //premium number
+global.owner = ['56977774748','447441419651'] //owner number, u can put multiple owner number, within quotations and seperated by comma.
+global.pemilik = ['50495745934'] //another owner number
+global.premium = ['56954940391'] //premium number
 global.pengguna = 'Xeon' //username
-global.botnma = 'Cheems Bot-MD' //bot name
-global.ownernma = '🦄Dream Guy Xeon' //owner name
-global.packname = 'Cheems Bot' //sticker package name
-global.author = 'By Dream Guy Xeon' //sticker author name
+global.botnma = 'the whatbots420' //bot name
+global.ownernma = 'el rebelde' //owner name
+global.packname = 'toma tu sticker' //sticker package name
+global.author = 'the whatbots420' //sticker author name
 global.sessionName = 'session' //session name
 global.prefa = ['#','!','/',''] //prefix
 global.sp = '⭔' //design
